ef4c606cb5ae9340dc7efce1edc07b52f00241b1
boblee821226 <441814246@qq.com>
2019-10-14 14:14:50 +0800

    收据打印，删行 更新表头金额。

---
+++ zulin/src/client/nc/ui/hkjt/zulin/sjdy/ace/action/BodyDelLineAction.java
@@ -0,0 +1,46 @@
+package nc.ui.hkjt.zulin.sjdy.ace.action;
+
+import hd.vo.pub.tools.PuPubVO;
+
+import java.util.ArrayList;
+
+import nc.bs.framework.common.NCLocator;
+import nc.itf.uap.IUAPQueryBS;
+import nc.jdbc.framework.processor.ArrayListProcessor;
+import nc.ui.pub.beans.MessageDialog;
+import nc.vo.pub.lang.UFBoolean;
+import nc.vo.pub.lang.UFDate;
+import nc.vo.pub.lang.UFDouble;
+
+public class BodyDelLineAction extends
+		nc.ui.pubapp.uif2app.actions.BodyDelLineAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7346328761197832169L;
+
+	@Override
+	public void doAction() {
+		
+		super.doAction();
+		
+		/**
+		 * 表头金额字段重新赋值
+		 */
+		UFDouble total_je = UFDouble.ZERO_DBL;
+		
+		int rowCount = getCardPanel().getBillModel().getRowCount();
+		
+		for(int i=0;i<rowCount;i++) {
+			UFDouble skje = PuPubVO.getUFDouble_NullAsZero(
+					getCardPanel().getBillModel().getValueAt(i, "skje"));	// 收款金额
+			total_je = total_je.add(skje);
+		}
+		
+		getCardPanel().getHeadItem("vdef03").setValue(total_je);	// 表头-合计金额
+		/***END***/
+		
+	}
+	
+}
