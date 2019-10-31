7ee10f5b96a78574bf990c1e1847358f8f7ad8a9
boblee821226 <441814246@qq.com>
2019-10-31 16:56:46 +0800

    公共参数类

---
+++ zulin/src/public/nc/itf/hkjt/IPub_data.java
@@ -0,0 +1,12 @@
+package nc.itf.hkjt;
+
+public class IPub_data {
+	
+	/**
+	 * 闭口合同 ，业务类型
+	 */
+	public static String BKHT_tradetype = "F3-Cxx-03";
+	public static String BKHT_tradetypeid = "1001N510000000AGSI18";
+	public static String BKHT_type = "1001N510000000AGYP1K";
+
+}
