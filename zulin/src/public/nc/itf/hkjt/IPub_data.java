21c49953f3d172c325629e9791023121ad1948a4
boblee821226 <441814246@qq.com>
2020-01-14 10:42:57 +0800

    资金计划项目 采购款 变了。

--- zulin/src/public/nc/itf/hkjt/IPub_data.java
+++ zulin/src/public/nc/itf/hkjt/IPub_data.java
@@ -14,8 +14,8 @@ public class IPub_data {
 	 * 结算生下拨申请
 	 */
 	public static String JSXB_done 			 = "已申请";
-	public static String JSXB_jhxm_caigou 	 = "1001N510000000007CGK";	// 资金计划项目：采购款
-	public static String JSXB_jhxm_gongcheng = "1001N510000000007CGL";	// 资金计划项目：工程款
+	public static String JSXB_jhxm_caigou 	 = "1001N510000000007CGW";	// 资金计划项目：采购款
+//	public static String JSXB_jhxm_gongcheng = "1001N510000000007CGL";	// 资金计划项目：工程款
 	public static String JSXB_pk_payorg		 = "0001N510000000001SXL";	// 下拨组织
 	public static String JSXB_pk_payorg_v	 = "0001N510000000001SXK";	// 下拨组织v
 	public static String JSXB_pk_balatype_ZZ = "0001Z0100000000000Y1";	// 转账
