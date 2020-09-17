1d8de53156835f41731e3519580dd8572a5352a9
boblee821226 <441814246@qq.com>
2020-09-17 17:39:13 +0800

    插件增加 是否启用的判断

--- store/src/private/nc/bs/hkjt/listener/SendLvyunDataListener.java
+++ store/src/private/nc/bs/hkjt/listener/SendLvyunDataListener.java
@@ -9,6 +9,7 @@ import nc.bs.businessevent.IBusinessEvent;
 import nc.bs.businessevent.IBusinessListener;
 import nc.bs.dao.BaseDAO;
 import nc.bs.ic.general.util.ICEventType;
+import nc.jdbc.framework.processor.ArrayListProcessor;
 import nc.jdbc.framework.processor.BeanListProcessor;
 import nc.vo.hkjt.store.lvyun.in.DeletePosStoreMasterWithDetail;
 import nc.vo.hkjt.store.lvyun.in.HttpUtil;
@@ -21,6 +22,7 @@ import nc.vo.ic.m4a.entity.GeneralInBodyVO;
 import nc.vo.ic.m4a.entity.GeneralInHeadVO;
 import nc.vo.ic.m4a.entity.GeneralInVO;
 import nc.vo.pub.BusinessException;
+import nc.vo.pub.lang.UFDate;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -29,6 +31,49 @@ public class SendLvyunDataListener implements IBusinessListener {
 	private static ObjectMapper JSON = new ObjectMapper();
 	private static String URL = "http://183.129.215.114:41104/pos/router";
 	private static BaseDAO DAO = new BaseDAO();
+	private static Boolean isUse = Boolean.TRUE;	// 是否启用
+	/**
+	 * key：单据id
+	 * value：审核时间的 时+分
+	 */
+	private static HashMap<String,String> MAP_audittime = new HashMap<>();
+	/**
+	 * 根据单据id查询审核时间：时+分
+	 */
+	private static String queryAuditTime(String id) throws BusinessException {
+		String result = null;
+		StringBuffer querySQL = 
+		new StringBuffer("select ")
+			.append(" taudittime ")
+			.append(" from ic_generalin_h ")
+			.append(" where cgeneralhid = '").append(id).append("' ")
+		;
+		// 2020-09-15 19:12:30 = 1912
+		ArrayList list = (ArrayList)DAO.executeQuery(querySQL.toString(), new ArrayListProcessor());
+		if (list != null && !list.isEmpty()) {
+			String auditTime = PuPubVO.getString_TrimZeroLenAsNull(((Object[])list.get(0))[0]);
+			result = getAuditTime(auditTime);
+		}
+		return result;
+	}
+	/**
+	 * 获取审核时间中的 时+分
+	 */
+	private static String getAuditTime(String auditTime) throws BusinessException {
+		String result = null;
+		if (auditTime != null && auditTime.length() == 19) {
+			result = auditTime.substring(11, 13) + auditTime.substring(14, 16);
+		}
+		return result;
+	}
+	/**
+	 * 获取审核时间中的 时+分
+	 */
+	private static String getAuditTime(UFDate auditTime) throws BusinessException {
+		String result = null;
+		result = getAuditTime(auditTime.toString());
+		return result;
+	}
 	
 	@Override
 	public void doAction(IBusinessEvent event) throws BusinessException {
@@ -36,6 +81,7 @@ public class SendLvyunDataListener implements IBusinessListener {
 		String eventType = e.getEventType();
 		
 		if (ICEventType.SignAfter.getCode().equals(eventType)) {
+			if(!isUse) return;	// 是否启用
 			// 签字后
 			for (Object billVoItem : e.getOldObjs()) {
 				GeneralInVO billVO = (GeneralInVO)billVoItem;
@@ -44,6 +90,7 @@ public class SendLvyunDataListener implements IBusinessListener {
 				String cwarehouseid = hVO.getCwarehouseid();	// 仓库
 				String pk_org = hVO.getPk_org();				// 组织
 				String billcode = hVO.getVbillcode();			// 单号
+				String billId = hVO.getCgeneralhid();			// ID
 				String dbilldateStr = PuPubVO.getString_TrimZeroLenAsNull(hVO.getDbilldate());			// 单据日期
 				if (!"4A-Cxx-01".equals(billTypeCode)) continue;	// 只有 4A-Cxx-01 才需要发送给绿云
 				// 判断仓库是否在 商品分类：营业点里 做过配置。（如果做过配置 就说明该仓库为绿云二级库，就需要发送）
@@ -78,15 +125,15 @@ public class SendLvyunDataListener implements IBusinessListener {
 					 */
 					StringBuffer querySQL = 
 					new StringBuffer("select ")
-							.append(" m.code ")		// 编码
-							.append(",m.name ")		// 名称
+							.append(" m.code ")			// 编码
+							.append(",m.name ")			// 名称
 							.append(",m.materialspec || m.materialtype as standent ")	// 规格
 							.append(",dw.name unit")	// 单位
 							.append(",m.def6 price ")	// 单价
 //							.append(",m.code helpCode ")	// 助记码
-//							.append(",'6002' sseg ")	// 小类code
-//							.append(",'6' cseg ")		// 大类code
-//							.append(",'ref' ref ")		// 备注
+//							.append(",'6002' sseg ")		// 小类code
+//							.append(",'6' cseg ")			// 大类code
+//							.append(",'ref' ref ")			// 备注
 //							.append(",'storage' storage ")	// 科目代码
 							.append(",m.pk_material pk ")	// 物料pk
 							.append(",m.def8 lyCode ")		// 绿云code
@@ -162,6 +209,7 @@ public class SendLvyunDataListener implements IBusinessListener {
 				// 入库单的发送
 				if (true) {
 					try {
+						String hhmi = getAuditTime(hVO.getTaudittime());
 						PosStoreDetail[] details = new PosStoreDetail[bVOs.length];
 						for (int i = 0; i < bVOs.length; i++) {
 							details[i] = new PosStoreDetail();
@@ -170,13 +218,13 @@ public class SendLvyunDataListener implements IBusinessListener {
 						}
 						SavePosStoreMasterWithDetail sendData = 
 							new SavePosStoreMasterWithDetail(
-								 "GCBZ"		// 酒店编码
-								,billcode		// 单号
-								,dbilldateStr	// 时间
-								,"003"	// 吧台（跟仓库匹配上？）
-								,"zwf"	// 操作员（如何找？）
-								,""		// 发票号
-								,""		// 备注
+								 "GCBZ"				// 酒店编码
+								,billcode + hhmi	// 单号
+								,dbilldateStr		// 时间
+								,"003"		// 吧台（跟仓库匹配上？）
+								,"zwf"		// 操作员（如何找？）
+								,""			// 发票号
+								,""			// 备注
 								,details	// 物料明细
 							);
 						String resStr = HttpUtil.doPost(URL, JSON.writeValueAsString(sendData));
@@ -194,21 +242,41 @@ public class SendLvyunDataListener implements IBusinessListener {
 				}
 			}
 //			throw new BusinessException("==签字后==测试");
+		} else if (ICEventType.CancelsignBefore.getCode().equals(eventType)) {
+			if(!isUse) return;	// 是否启用
+			// 取消签字前
+			for (Object item : e.getOldObjs()) {
+				GeneralInVO billVO = (GeneralInVO)item;
+				GeneralInHeadVO hVO = (GeneralInHeadVO)billVO.getParentVO();	//  表头
+				String billTypeCode = hVO.getVtrantypecode();	// 入库类型
+				String billId = hVO.getCgeneralhid();			// ID
+				if (!"4A-Cxx-01".equals(billTypeCode)) continue;	// 只有 4A-Cxx-01 才需要发送给绿云
+				if (true) {
+					// 查询 审核时间
+					String hhmi = queryAuditTime(billId);
+					// 放到缓存里
+					MAP_audittime.put(billId, hhmi);
+				}
+			}
+			// throw new BusinessException("==取消签字后==");
 		} else if (ICEventType.CancelsignAfter.getCode().equals(eventType)) {
+			if(!isUse) return;	// 是否启用
 			// 取消签字后
 			for (Object item : e.getOldObjs()) {
 				GeneralInVO billVO = (GeneralInVO)item;
 				GeneralInHeadVO hVO = (GeneralInHeadVO)billVO.getParentVO();	//  表头
 				String billTypeCode = hVO.getVtrantypecode();	// 入库类型
 				String billcode = hVO.getVbillcode();			// 单号
+				String billId = hVO.getCgeneralhid();			// ID
 				if (!"4A-Cxx-01".equals(billTypeCode)) continue;	// 只有 4A-Cxx-01 才需要发送给绿云
 				// 删除 入库单发送
 				if (true) {
 					try {
+						String hhmi = MAP_audittime.remove(billId);
 						DeletePosStoreMasterWithDetail sendData = 
 							new DeletePosStoreMasterWithDetail(
-								"GCBZ"		// 酒店编码
-								,billcode		// 单号
+								"GCBZ"				// 酒店编码
+								,billcode + hhmi	// 单号 + hhmi
 						);
 						String resStr = HttpUtil.doPost(URL, JSON.writeValueAsString(sendData));
 						Result res = JSON.readValue(resStr, Result.class);
