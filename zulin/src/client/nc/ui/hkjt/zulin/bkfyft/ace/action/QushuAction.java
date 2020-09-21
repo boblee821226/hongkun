550c8f196cedbcb15c8c63007b4ef4695ea2b4f9
boblee821226 <441814246@qq.com>
2020-09-21 19:57:02 +0800

    分摊截止日期的优化

--- zulin/src/client/nc/ui/hkjt/zulin/bkfyft/ace/action/QushuAction.java
+++ zulin/src/client/nc/ui/hkjt/zulin/bkfyft/ace/action/QushuAction.java
@@ -117,7 +117,15 @@ public class QushuAction extends NCAction {
 				.append(",szxm.name vdef03 ")	// 支出项目name
 				.append(",substr(htb.vbdef3,1,10) ksrq ")	// 合同开始日期
 //				.append(",case when ht.actualinvalidate is null then substr(htb.vbdef4,1,10) else substr(ht.actualinvalidate,1,10) end jsrq ")	// 合同结束日期
-				.append(",case when nvl(ht.invallidate,'~') = '~' then substr(htb.vbdef4,1,10) else substr(ht.invallidate,1,10) end jsrq ")	// 合同结束日期
+				.append(",case when nvl(ht.invallidate,'~') = '~' then substr(htb.vbdef4,1,10) " +
+						" else " +
+						/**
+						 * HK 2020年9月21日19:55:02
+						 * 如果有 分摊截止日期，并且 小于 表体结束日期，则以 分摊截止日期 为准。
+						 */
+//						" 	substr(ht.invallidate,1,10) " +
+						"	(case when ht.invallidate < htb.vbdef4 then substr(ht.invallidate, 1, 10) else substr(htb.vbdef4, 1, 10) end) " +
+						" end jsrq ")	// 合同结束日期
 				.append(",htb.norigmny vdef11 ")	// 无税金额
 				.append(",htb.norigtaxmny vdef12 ")	// 含税金额
 				.append(",ht.depid vdef05 ")	// 部门pk
@@ -181,7 +189,7 @@ public class QushuAction extends NCAction {
 				.append(" and '"+str_yb_ksrq+"' <= substr(nvl(replace(ht.invallidate, '~', ''), '2099-12-31'), 1, 10) ")	// 合同终止日期 来 判断计费时点（终止的那天 要算租金的）
 				.append(" and substr(htb.vbdef3,1,10) <= substr(nvl(replace(ht.invallidate, '~', ''), '2099-12-31'), 1, 10) ")	// 合同明细的开始日期 要小于等于 合同终止日期
 				.append(whereSql_zeren) // 责任凭证的sql
-//				.append(" and ht.vbillcode like '050820190801%' ")	// 测试
+//				.append(" and ht.vbillcode like '04162019023879%' ")	// 测试
 		;
 		
 		IUAPQueryBS iUAPQueryBS = (IUAPQueryBS)NCLocator.getInstance().lookup(IUAPQueryBS.class.getName()); 
